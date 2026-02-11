d5bafc4a96dd407f8a143a9043c504b5e87e350c
Youana Habib <152451238+youanahabib117@users.noreply.github.com>
2026-02-10 22:15:10 -0600

    Delete db.py

--- db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-import os
-
-def get_engine():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.join(base_dir, "business.db")
-    return create_engine(f"sqlite:///{db_path}")
